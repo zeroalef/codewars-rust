0a8276c68ae6b83188e6589b57ed23ee65385a14
zeroalef <zeroalef@protonmail.com>
2019-04-12 17:04:07 +0300

    refactor

--- your_order/src/lib.rs
+++ your_order/src/lib.rs
@@ -1,37 +1,5 @@
-#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
-struct KeyValue {
-    key: u32,
-    value: String,
-}
-
-impl KeyValue {
-    pub fn new(key: u32, value: String) -> Self {
-        KeyValue { key, value }
-    }
-}
-
 pub fn order(sentence: &str) -> String {
-    if sentence.is_empty() {
-        sentence.to_string()
-    } else {
-        let foo = |s: &str| -> Vec<KeyValue> {
-            s.split(' ')
-                .map(|s| {
-                    KeyValue::new(
-                        s.chars().fold(0, |sum, ch| match ch.to_digit(10) {
-                            Some(i) => sum + i,
-                            None => sum,
-                        }),
-                        s.to_string(),
-                    )
-                })
-                .collect::<Vec<KeyValue>>()
-        };
-        let mut tmp = foo(sentence);
-        tmp.sort_by(|a, b| a.key.cmp(&b.key));
-        tmp.iter()
-            .map(|item| item.value.clone())
-            .collect::<Vec<String>>()
-            .join(" ")
-    }
+    let mut ws: Vec<_> = sentence.split_whitespace().map(String::from).collect();
+    ws.sort_by_key(|s| s.chars().find(|c| c.is_digit(10)).unwrap());
+    ws.join(" ")
 }
