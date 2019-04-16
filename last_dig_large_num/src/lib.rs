fb0c5a63b68513d97121edb6907c71c82007eb10
zeroalef <zeroalef@protonmail.com>
2019-04-16 19:41:23 +0300

    well perfect algorythm

--- last_dig_large_num/src/lib.rs
+++ last_dig_large_num/src/lib.rs
@@ -1,45 +1,16 @@
 #[cfg(test)]
 mod tests {
-    extern crate num;
-
-    use num::bigint::BigInt;
-    use num::ToPrimitive;
-    use std::ops::Rem;
-    use std::str::FromStr;
-
+    
     fn last_digit(str1: &str, str2: &str) -> i32 {
         if str2.chars().all(|ch| (ch == '0')) {
             1
         } else if str1.chars().all(|ch| (ch == '0')) {
             0
         } else {
-            match str1.chars().last() {
-                Some('5') => 5,
-                Some('6') => 6,
-                Some('0') => 0,
-                Some('1') => 1,
-                Some(ch) => {
-                    let f = |ch: char| -> i32 {
-                        let n: BigInt = BigInt::from_str(str2).unwrap().rem(4);
-                        let k: usize = n.to_usize().unwrap();
-                        let ret_even: Vec<Vec<i32>> =
-                            vec![vec![6, 2, 4, 8], vec![6, 4, 6, 4], vec![6, 8, 4, 2]];
-                        let ret_odd: Vec<Vec<i32>> =
-                            vec![vec![1, 3, 9, 7], vec![1, 7, 9, 3], vec![1, 9, 1, 9]];
-                        match ch {
-                            '2' => ret_even[0][k],
-                            '4' => ret_even[1][k],
-                            '8' => ret_even[2][k],
-                            '3' => ret_odd[0][k],
-                            '7' => ret_odd[1][k],
-                            '9' => ret_odd[2][k],
-                            _ => -1,
-                        }
-                    };
-                    f(ch)
-                }
-                None => -1,
-            }
+            let x = str1.chars().last().unwrap().to_digit(10).unwrap();
+            let m = str2.chars().fold(0, |a,x| (a*10 + x.to_digit(10).unwrap()) % 4);
+            let exp = if m == 0 { 4 } else { m };
+            (x.pow(exp) % 10) as i32
         }
     }
 
